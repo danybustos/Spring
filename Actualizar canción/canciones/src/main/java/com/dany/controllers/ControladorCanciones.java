f54f6a54dbfa4641496bfe3754cafde8f5242939
Daniela Bustos <daniscar.bustos@gmail.com>
2024-11-22 18:59:36 -0300

    commit1

---
+++ Actualizar canción/canciones/src/main/java/com/dany/controllers/ControladorCanciones.java
@@ -0,0 +1,66 @@
+package com.dany.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import com.dany.models.Cancion;
+import com.dany.services.ServicioCanciones;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class ControladorCanciones {
+
+    @Autowired
+    private ServicioCanciones servicioCanciones;
+
+    @GetMapping("/canciones")
+    public String desplegarCanciones(Model modelo) {
+        modelo.addAttribute("canciones", servicioCanciones.obtenerTodasLasCanciones());
+        return "canciones.jsp";
+    }
+
+    @GetMapping("/canciones/detalle/{idCancion}")
+    public String desplegarDetalleCancion(@PathVariable("idCancion") Long id, Model modelo) {
+        Cancion cancion = servicioCanciones.obtenerCancionPorId(id);
+        modelo.addAttribute("cancion", cancion);
+        return "detalleCancion.jsp";
+    }
+
+    @GetMapping("/canciones/formulario/agregar/{idCancion}")
+    public String formularioAgregarCancion(@PathVariable("idCancion") Long id, Model modelo) {
+        modelo.addAttribute("cancion", new Cancion());
+        return "agregarCancion.jsp";
+    }
+
+    @PostMapping("canciones/procesa/agregar")
+    public String procesarAgregarCancion(@Valid @ModelAttribute("cancion") Cancion cancion, BindingResult result) {
+        if (result.hasErrors()) {
+            return "agregarCancion.jsp";
+        }
+        servicioCanciones.agregarCancion(cancion);
+        return "redirect:/canciones";
+    }
+
+    @GetMapping("/canciones/formulario/editar/{idCancion}")
+    public String formularioEditarCancion(@PathVariable("idCancion") Long id, @ModelAttribute("cancion") Cancion cancion, Model modelo) {
+        modelo.addAttribute("cancion", servicioCanciones.obtenerCancionPorId(id));
+        return "editarCancion.jsp";
+    }
+
+    @PutMapping("/canciones/procesa/editar/{idCancion}")
+    public String procesarEditarCancion(@Valid @ModelAttribute("cancion") Cancion cancion, BindingResult result, @PathVariable("idCancion") Long id) {
+        if (result.hasErrors()) {
+            return "editarCancion.jsp";
+        }
+        servicioCanciones.actualizarCancion(cancion);
+        return "redirect:/canciones";
+    }
+}
---
+++ Actualizar canción/canciones/src/main/webapp/WEB-INF/agregarCancion.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; UTF-8"	pageEncoding="UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Agregar Cancion</title>
+</head>
+<body>
+    <h1>¡Agrega tu canción!</h1>
+    <form:form action="/canciones/procesa/agregar" method="POST" modelAttribute="cancion">
+        <form:label path="titulo">Título:</form:label>
+        <form:input type="text" path="titulo"/>
+        <form:errors path="titulo" cssClass="error"/>
+
+        <form:label path="artista">Artista:</form:label>
+        <form:input type="text" path="artista"/>
+        <form:errors path="artista" cssClass="error"/>
+
+        <form:label path="album">Álbum:</form:label>
+        <form:input type="text" path="album"/>
+        <form:errors path="album" cssClass="error"/>
+
+        <form:label path="genero">Género:</form:label>
+        <form:input type="text" path="genero"/>
+        <form:errors path="genero" cssClass="error"/>
+
+        <form:label path="idioma">Idioma:</form:label>
+        <form:input type="text" path="idioma"/>
+        <form:errors path="idioma" cssClass="error"/>
+
+        <button type="submit">Guardar Canción</button>
+    </form:form>
+    <a href="/canciones">Volver a lista de canciones</a>
+</body>
+</html>
